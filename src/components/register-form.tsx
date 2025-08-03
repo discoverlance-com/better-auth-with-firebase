957a0bd81c9f5295402347a08130930f4e186d8a
Lance Armah-Abraham <lance@vdc.cloud>
2025-08-03 12:39:24 +0000

    feat: setup register page and add shadcn form

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -13,7 +13,7 @@ import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { siteLinks } from "@/config/site";
 
-export function LoginForm({
+export function RegisterForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
@@ -21,9 +21,9 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">Welcome back</CardTitle>
+          <CardTitle className="text-xl">Get Started</CardTitle>
           <CardDescription>
-            Login with your Github or Google account
+            Register with your Github or Google account
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -37,7 +37,7 @@ export function LoginForm({
                       d="M12 0.297C5.373 0.297 0 5.67 0 12.297c0 5.284 3.438 9.77 8.207 11.363.6.11.793-.26.793-.577v-2.23c-3.338.726-4.033-1.61-4.033-1.61-.546-1.387-1.333-1.757-1.333-1.757-1.09-.745.082-.73.082-.73 1.206.085 1.84 1.237 1.84 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.42-1.305.763-1.604-2.665-.304-5.467-1.332-5.467-5.93 0-1.31.468-2.382 1.236-3.222-.124-.303-.536-1.524.118-3.176 0 0 1.008-.322 3.3 1.23a11.51 11.51 0 0 1 3-.404c1.02.005 2.045.138 3 .404 2.29-1.552 3.296-1.23 3.296-1.23.656 1.652.244 2.873.12 3.176.77.84 1.233 1.912 1.233 3.222 0 4.61-2.807 5.624-5.48 5.922.432.372.816 1.103.816 2.222v3.293c0 .32.192.694.8.576C20.565 22.063 24 17.577 24 12.297 24 5.67 18.627.297 12 .297z"
                     />
                   </svg>
-                  Login with Github
+                  Register with Github
                 </Button>
                 <Button variant="outline" className="w-full">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -46,7 +46,7 @@ export function LoginForm({
                       fill="currentColor"
                     />
                   </svg>
-                  Login with Google
+                  Register with Google
                 </Button>
               </div>
               <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
@@ -57,36 +57,28 @@ export function LoginForm({
               <div className="grid gap-6">
                 <div className="grid gap-3">
                   <Label htmlFor="email">Email</Label>
+                  <Input id="email" type="email" required />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="pasword">Password</Label>
                   <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
+                    id="password"
+                    type="password"
+                    autoComplete="new-password"
                     required
                   />
                 </div>
-                <div className="grid gap-3">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <Link
-                      href={siteLinks.forgotPassword}
-                      className="ml-auto text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </Link>
-                  </div>
-                  <Input id="password" type="password" required />
-                </div>
                 <Button type="submit" className="w-full">
-                  Login
+                  Register
                 </Button>
               </div>
               <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
+                Already have an account?{" "}
                 <Link
-                  href={siteLinks.signup}
+                  href={siteLinks.signin}
                   className="underline underline-offset-4"
                 >
-                  Register
+                  Login
                 </Link>
               </div>
             </div>
